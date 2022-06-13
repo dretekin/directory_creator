942b290c8251513230a4a4c909fa8e9a1910bfd5
dretekin <dretekin@gmail.com>
2022-06-13 15:57:24 +0000

    directory creator

---
+++ dirManager.js
@@ -0,0 +1,36 @@
+
+var fs = require("fs");
+var path = require("path");
+var readline = require("readline");
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+dirManager();
+function dirManager() {
+  rl.question("Please, enter your new directory path? ", function (newDirPath) {
+    fs.mkdir(newDirPath, function (err) {
+      if (err) {
+        if (err.code == "EEXIST") {
+          rl.question(
+            "The directory you want to create already exist.Do you want to create a different directory? (Y / n) ",
+            function (data) {
+              data = data.toString().toLowerCase();
+              if (data == "y") {
+                dirManager();
+              } else {
+                process.exit();
+              }
+            }
+          );
+        }
+      } else {
+        fs.readdir("./", function (err, data) {
+          console.log(data);
+        });
+        process.exit();
+      }
+    });
+  });
+}
